87b6f4b569b01ce6524d3ddf419f25b152973555
Lucca Kaiser <luccakaiser@gmail.com>
2019-01-22 17:01:04 +0100

    add head and header partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'by Dojo Madness 👾'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
