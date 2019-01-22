cb5b77d518b4d31a8d72257dedd7874b47f93e34
Lucca Kaiser <luccakaiser@gmail.com>
2019-01-22 16:54:29 +0100

    finish search feature

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -3,4 +3,8 @@ class Hero < ApplicationRecord
   validates :portrait, presence: true, uniqueness: true
   validates :background, presence: true, uniqueness: true
   validates :link, presence: true
+
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%")
+  end
 end
